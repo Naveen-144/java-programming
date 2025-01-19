8254427ac88cebacb03bbb1da67e38f78741caa3
Naveen-144 <naveenk144410@gmail.com>
2025-01-19 22:33:56 +0530

    string scanner

---
+++ stringscan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class stringscan {
+    public static void main(String args[])
+     {
+         try (Scanner nav = new Scanner(System.in)) {
+            String a = nav.nextLine();
+            
+            System.out.print(a);
+            
+        }
+        
+    }
+}
