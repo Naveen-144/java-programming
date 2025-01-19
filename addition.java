59ee73ac20f974b35cd33ceff42d0d057e96e35e
Naveen-144 <naveenk144410@gmail.com>
2025-01-19 22:00:45 +0530

    addition program

---
+++ addition.java
@@ -0,0 +1,13 @@
+class addition
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c=30;
+        int d = a+b+c;
+        System.out.print(d);
+    }
+
+
+}
