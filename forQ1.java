fa2b01f75f6d5b1eda5207bded7849df08b3c644
Naveen-144 <naveenk144410@gmail.com>
2025-01-25 22:09:14 +0530

    terenary operator

---
+++ forQ1.java
@@ -0,0 +1,13 @@
+public class forQ1 {
+    
+    public static void main(String[] args) {
+        int i = 1;
+        for(i=1;i<=5;i++){
+            for(int j=i;j>=5-1;j++){
+            System.out.println(" ");
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
---
+++ ternaryoperator.java
@@ -0,0 +1,15 @@
+// Syntax
+// variable = (condition)?valueiftrue:valueiffalse
+public class ternaryoperator {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        String a = 5>2?"yes":"no";
+        System.out.println(a);
+        int b = 5<2?0:1;
+        System.out.println(b);
+
+
+    }
+
+    
+}
